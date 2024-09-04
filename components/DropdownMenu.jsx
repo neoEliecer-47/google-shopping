e91c62f341cb421f80adfec859d3c2b96cd6c468
Eliecer <eliecer.sanchez47@gmail.com>
2024-09-03 22:44:10 -0400

    tooltip added for longest placeholders

--- components/DropdownMenu.jsx
+++ components/DropdownMenu.jsx
@@ -1,18 +1,18 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import styles from "./DropdownMenu.module.css";
 import { CheckIcon } from "@heroicons/react/24/solid";
 import { ChevronDownIcon } from "@heroicons/react/24/outline";
 import classNames from "classnames";
 import { useClickOutsideDetector } from "@/hooks/useClickOutsideDetector";
-import TooltipInfo from "./UI/TooltipInfo";
 
 export default function DropdownMenu({ placeholder, data, money = false }) {
   const [open, setOpen] = useState(false);
   const [option, setOption] = useState(null);
-  
 
-  const { dropMenuRef, isClickOutside, setIsClickOutside } =
-    useClickOutsideDetector();
+
+  const { dropMenuRef, isClickOutside, setIsClickOutside } = useClickOutsideDetector();
 
   function handleOpenOptions(e) {
     e.stopPropagation();
@@ -23,21 +23,16 @@ export default function DropdownMenu({ placeholder, data, money = false }) {
     if (money && index === 0) {
       return "No Minimun";
     }
-    if(item instanceof Object){
-      const element = Object.values(item)[0]
-      console.log(element)
-      return element
-    }
-    
-    return item
-    
+    // if(item instanceof Object)
+
+    return typeof item === "object" ? Object.values(item)[0] : item;
   }
 
-  function buildPlaceholder() {
+  function buildPlaceholder(tooltip) {
     let item = data[option];
     let placeholder = "";
     placeholder = getValues(item, option);
-    
+    if (tooltip) return placeholder;
     return placeholder.slice(0, 10) + `${placeholder.length > 10 ? "..." : ""}`;
   }
 
@@ -53,14 +48,21 @@ export default function DropdownMenu({ placeholder, data, money = false }) {
     <section style={{ width: "8rem", cursor: "pointer" }} ref={dropMenuRef}>
       
         
-      
-        
-     
-      <div
-        className={classNames(styles.placeholderContainer, option && styles.placeholderContainerSelected)}
+
+
+<div
+        className={classNames(
+          styles.placeholderContainer,
+          option && styles.placeholderContainerSelected
+        )}
         onClick={handleOpenOptions}
-        
       >
+        {buildPlaceholder('tooltip')?.length > 10 && (
+          <div className={styles.tooltipContainer}>
+          <span className={""}>{buildPlaceholder('tooltip')}</span>
+        </div>
+        )}
+
         <h2 className={styles.placeholder}>
           {option === null ? placeholder : buildPlaceholder()}
         </h2>
@@ -68,6 +70,7 @@ export default function DropdownMenu({ placeholder, data, money = false }) {
           className={classNames(styles.icon, open && styles.iconRotate)}
         />
       </div>
+
       <div
         className={styles.options}
         style={{ padding: `${open ? "10px 0px" : "0px"}`, width: "8rem" }}
@@ -80,7 +83,6 @@ export default function DropdownMenu({ placeholder, data, money = false }) {
                 display: "flex",
                 justifyContent: "space-between",
                 alignItems: "center",
-               
               }}
               onClick={() => setOption(index)}
               className={styles.containerO}
@@ -88,7 +90,7 @@ export default function DropdownMenu({ placeholder, data, money = false }) {
               <div style={{ width: "1.3rem" }}>
                 {option === index && <CheckIcon className={styles.icon} />}
               </div>
-              <div className={styles.optionButton} >
+              <div className={styles.optionButton}>
                 {money && index !== 0 && "$"}
                 {getValues(item, index)}
               </div>
